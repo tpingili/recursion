1bdadee4b4ef5ee521bb151ab87985aaa2cbdf4e
tpingili <ptejaswini.reddy@gmail.com>
2014-07-21 16:09:34 -0700

    Implemented stringifyJSON.

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,5 +3,36 @@
 
 // but you don't so you're going to write it from scratch:
 var stringifyJSON = function(obj) {
-  // your code goes here
+  //Wrap strings in quotes as per JSON specs
+  if(typeof obj === 'string'){
+    return '"'+obj+'"';
+  }
+  //temp array used to hold Array and Object results.
+  var result =[];
+
+  //Check if obj is an array
+  if(Array.isArray(obj)){
+    //Iterate over each element of the array
+    for(var i = 0; i < obj.length; i++ ){
+      //Stringify element and add to result array.
+      result.push(stringifyJSON(obj[i]));
+    }
+    //wrap the array with the square brackets and convert it into a string
+    return '[' + result + ']';
+  }
+  //check if obj is an object and not null
+  if(typeof obj === 'object' && obj !== null){
+    //iterate through each key value pair
+    for(var key in obj){
+      //if the value is not a function or undefined
+      if(!(typeof obj[key] === 'function' || obj[key] === undefined)){
+        //stringify the key and value and add it to result array
+        result.push(stringifyJSON(key) + ':' + stringifyJSON(obj[key]));
+      }
+    }
+    //wrap the array with curly brackets and convert it into a string
+    return '{' + result + '}';
+  }
+  //return obj coerced into a string
+  return ''+obj;
 };
